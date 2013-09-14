74ad950d799cead7ace26b5ec2cdbad3ebc4f24e
Rohan Kapadia <rohan@gharpay.in>
2013-09-14 18:03:43 +0530

    initial commit.

---
+++ index.js
@@ -0,0 +1,13 @@
+var express = require("express");
+var app = express();
+var port = 3700;
+ 
+app.set('views', __dirname + '/tpl');
+app.set('view engine', "jade");
+app.engine('jade', require('jade').__express);
+app.get("/", function(req, res){
+    res.render("page");
+});
+ 
+app.listen(port);
+console.log("Listening on port " + port);
