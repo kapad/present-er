7afea1761982ea26fc285fbe27ee47a0852b871d
Rohan Kapadia <rohan@gharpay.in>
2013-09-15 11:18:57 +0530

    images loading and staying hidden. Things not centering.

--- public/js/presentation-viewer.js
+++ public/js/presentation-viewer.js
@@ -1,27 +1,46 @@
-window.onload = function() {
+// window.onload = function() {
 
-    // var messages = [];
-    // var field = document.getElementById("field");
-    // var sendButton = document.getElementById("send");
-    // var content = document.getElementById("content");
+//     // var messages = [];
+//     // var field = document.getElementById("field");
+//     // var sendButton = document.getElementById("send");
+//     // var content = document.getElementById("content");
  
-    // socket.on('message', function (data) {
-    //     console.log("got message");
-    //     if(data.message) {
-    //         console.log("got message with data");
-    //         messages.push(data.message);
-    //         var html = '';
-    //         for(var i=0; i<messages.length; i++) {
-    //             html += messages[i] + '<br />';
-    //         }
-    //         content.innerHTML = html;
-    //     } else {
-    //         console.log("There is a problem:", data);
-    //     }
-    // });
+//     // socket.on('message', function (data) {
+//     //     console.log("got message");
+//     //     if(data.message) {
+//     //         console.log("got message with data");
+//     //         messages.push(data.message);
+//     //         var html = '';
+//     //         for(var i=0; i<messages.length; i++) {
+//     //             html += messages[i] + '<br />';
+//     //         }
+//     //         content.innerHTML = html;
+//     //     } else {
+//     //         console.log("There is a problem:", data);
+//     //     }
+//     // });
     
-    // declare the vars from the DOM here.
+//     // declare the vars from the DOM here.
     
-    var socket = io.connect('http://localhost:3700');
+//     $("#main").
+    
+//     var socket = io.connect('http://localhost:3700');
 
-}
+// }
+(function()
+{
+    $(document).ready(function()
+    {
+        // var canvas = $("#main").get();
+        // function resizeCanvas()
+        // {
+        //     canvas.width = window.innerWidth*0.98;
+        //     canvas.height = window.innerHeight*0.98;
+        // }
+        // resizeCanvas();
+        // $(window).resize(function()
+        // {
+        //     resizeCanvas();
+        // });
+    });
+})();
